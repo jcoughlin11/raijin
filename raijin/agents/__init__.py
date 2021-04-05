3eddefab7cab516ed1adf35ed1a8fd59eac5d86e
Jared Coughlin <jcoughl3@illinois.edu>
2021-04-05 10:48:23 -0500

    Adds RandomAgent.
    Adds random_agent to __init__.py.

--- raijin/agents/__init__.py
+++ raijin/agents/__init__.py
@@ -1,4 +1,5 @@
 from . import (
     base_agent,
     qagent,
+    random_agent,
 )
---
+++ raijin/agents/random_agent.py
@@ -0,0 +1,74 @@
+from gym import Env
+from omegaconf.dictconfig import DictConfig
+import torch
+
+from raijin.memory.experience import Experience
+from raijin.pipelines import base_pipeline as bp
+
+from .base_agent import BaseAgent
+
+
+# ============================================
+#                 RandomAgent
+# ============================================
+class RandomAgent(BaseAgent):
+    """
+    An agent that performs only randomly selected actions. 
+
+    * Progresses through the game frame-by-frame
+    * Employs an epsilon-greedy strategy for action selection
+    """
+
+    __name__ = "RandomAgent"
+
+    # -----
+    # constructor
+    # -----
+    def __init__(
+        self, env: Env, pipeline: "bp.BasePipeline", params: DictConfig
+    ) -> None:
+        self.env = env
+        self.pipeline = pipeline
+        self.state = None
+
+    # -----
+    # reset
+    # -----
+    def reset(self) -> None:
+        """
+        Reverts the environment back to its initial state.
+        """
+        frame = self.env.reset()
+        self.state = self.pipeline.process(frame, True)
+
+    # -----
+    # choose_action
+    # -----
+    def choose_action(self, actionChoiceType: str) -> int:
+        """
+        Selects a random action.
+        """
+        return self.env.action_space.sample()
+
+    # -----
+    # step
+    # -----
+    def step(self, actionChoiceType: str, net: torch.nn.Module) -> Experience:
+        """
+        Transition from one game frame to the next.
+        """
+        action = self.choose_action("explore")
+        nextFrame, reward, done, _ = self.env.step(action)
+        nextState = self.pipeline.process(nextFrame, False)
+        experience = Experience(self.state, action, reward, nextState, done)
+        if done:
+            self.reset()
+        else:
+            self.state = nextState
+        return experience
+
+    # -----
+    # state_dict
+    # -----
+    def state_dict(self) -> dict:
+        return {}
